287e7b0e8cfc6837adc1913c9f8f6be8d43ae68a
YaroslavYatyk <duhanov2003@gmail.com>
2021-11-17 16:34:23 +0200

    added admin pannel

--- order_ticket/admin.py
+++ order_ticket/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
+from order_ticket.models import Discount, OrderTicket, RailCarType, Railcar, Ticket
+
 # Register your models here.
+
+@admin.register(Railcar)
+class RailcarAdmin(admin.ModelAdmin):
+    list_display = ('train','type_of_railcar', "number")
+    fields = ('type_of_railcar', "train", "number", "amount_of_seats")
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('discount_code','discount')
+    fields = ('discount_code','discount')
+
+
+@admin.register(RailCarType)
+class RailCarTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name',)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('train', "route", "railcar_number", "seat", "purchase_date", "is_taken")
+    fields = ('train', "route", "railcar_number", "seat", "purchase_date", "departure_date", "is_taken")
+
+
+@admin.register(OrderTicket)
+class OrderTicketAdmin(admin.ModelAdmin):
+    list_display = ('ticket', "start_point", "end_point", "customer_name", "customer_surname", )
+    readonly_fields = ('ticket', "start_point", "end_point", "customer_name", "customer_surname", "customer_patronymic",
+    "discount", "price", "email")
+    fields = ('ticket', "start_point", "end_point", "customer_name", "customer_surname", "customer_patronymic",
+    "discount", "price", "email")
+
--- search_ticket/admin.py
+++ search_ticket/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from search_ticket.models import Route, RouteStation, Station, Train
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('start_point','end_point', "train")
+    fields = ('start_point','end_point', "train", "slug")
+
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+    fields = ('name',)
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('station', "number_of_railcar")
+    fields = ('station', "number_of_railcar")
+
+
+@admin.register(RouteStation)
+class RouteStationAdmin(admin.ModelAdmin):
+    list_display = ('route', "time", "price_from_start")
+    fields = ('route', "stations", "time",  "price_from_start")
