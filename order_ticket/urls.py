89c0c7c02aa41aad81f01c034d25461a59f495f1
YaroslavYatyk <duhanov2003@gmail.com>
2021-12-18 16:32:44 +0200

    added order railcar and seat

--- order_ticket/urls.py
+++ order_ticket/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from order_ticket.views import OrderSeat, OrderRailcar
+from order_ticket.views import order_seat, order_railcar, OrderTicket
 
 
 urlpatterns = [
-    path("routs_for_user/<rout_slug>/<railcar>/", OrderSeat.as_view() , name="order_ticket"),
-    path("routs_for_user/<rout_slug>/", OrderRailcar.as_view() , name="order_railcar")
+    path("routs_for_user/<rout_slug>/<railcar>/", order_seat , name="order_seat"),
+    path("routs_for_user/<rout_slug>/", order_railcar , name="order_railcar"),
+    path("routs_for_user/<rout_slug>/<railcar>/<seat>/", OrderTicket.as_view() , name="order_tiket"),
 
 ]
--- order_ticket/views.py
+++ order_ticket/views.py
@@ -1,64 +1,44 @@
-from order_ticket.services.database.order_seat import fill_tickets
+# from order_ticket.services.database.order_seat import fill_tickets
+from django.http import HttpResponseRedirect
 from search_ticket.utils import DataMixin
 from icecream import ic
 from order_ticket.models import Ticket
 from search_ticket.models import Route
 from django.views.generic.edit import FormView
-from order_ticket.forms import OrderSeatForm, OrderRailcarForm
+from django.shortcuts import render
+from django.urls import reverse
 
 
-class OrderSeat(DataMixin, FormView):
-    """all avaluable routs according
-    to parameters
-    """
+def order_railcar(request, rout_slug):
 
+    if request.method == "POST":
+        railcar = request.POST.get("railcar")
+        ic(rout_slug, railcar)
+        return HttpResponseRedirect(reverse("order_seat", kwargs={"rout_slug": rout_slug, "railcar":railcar}))
+    ic(rout_slug)
+    print("here")
+    return render(request, "order_ticket/choose_place.html", context={"route_slug" : rout_slug})
+
+
+def order_seat(request, rout_slug, railcar):
+
+    if request.method == "POST":
+        seat = request.POST.get("seat")
+        return HttpResponseRedirect(reverse("order_tiket", kwargs={"rout_slug": rout_slug, "railcar":railcar, "seat" : seat}))
+    ic(rout_slug)
+    print("here")
+    return render(request, "order_ticket/order_seat.html", context={"route_slug" : rout_slug, "railcar": railcar})
+
+
+class OrderTicket(FormView):
+    
     model = Ticket
-    template_name = "order_ticket/choose_place.html"
+    template_name = "order_ticket/order_ticket.html"
 
     def get_queryset(self):
 
         return Ticket.objects.all()
 
     def get_context_data(self, **kwargs):
-        rout_slug = self.kwargs.get("rout_slug")
-        self.route = Route.objects.filter(slug=rout_slug).first()
-        form = OrderSeatForm(self.route, self.request.POST)
-        context = {"form": form}
-
-        if form.is_valid():
-            select_date = self.request.POST
-            print(select_date)
-
-        return context
-
-
-class OrderRailcar(DataMixin, FormView):
-    """all avaluable routs according
-    to parameters
-    """
-
-    model = Ticket
-    template_name = "order_ticket/choose_place.html"
-
-    def get_queryset(self):
-
-        rout_slug = self.kwargs.get("rout_slug")
-        self.route = Route.objects.filter(slug=rout_slug).first()
-
-        if not Ticket.objects.all():
-            fill_tickets(self.route)
-
-        if Ticket.objects.all():
-            ic(True)
-        return Ticket.objects.all()
-
-    def get_context_data(self, **kwargs):
-
-        rout_slug = self.kwargs.get("rout_slug")
-        self.route = Route.objects.filter(slug=rout_slug).first()
-        form = OrderRailcarForm(self.route, self.request.POST)  # <- remove this line
-        
-
-        context = {"form": form}
-
+        context = {"title" : "order"}
         return context
