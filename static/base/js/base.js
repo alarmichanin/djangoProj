d4d9f56dfdcfa33a3a0635c2e0f9fa433fb8a790
Vitala Kovalov <vitalya.kovalov@gmail.com>
2021-12-19 22:17:51 +0200

    Fix base page

--- static/base/js/base.js
+++ static/base/js/base.js
@@ -1,3 +1,8 @@
 M.AutoInit();
 
 M.updateTextFields();
+
+document.getElementById('nav-burger-menu').onclick = () => {
+    const navList = document.getElementById('nav-list');
+    navList.style.marginTop = navList.style.marginTop == '0px' ? '-310px' : '0px'; 
+};
