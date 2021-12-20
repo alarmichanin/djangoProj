2a9d84eb4982a86dc8e9aa8321130958bb4e6141
Vitala Kovalov <vitalya.kovalov@gmail.com>
2021-12-20 21:05:18 +0200

    Ticket info page and other fix witj front

--- static/base/js/base.js
+++ static/base/js/base.js
@@ -6,3 +6,7 @@ document.getElementById('nav-burger-menu').onclick = () => {
     const navList = document.getElementById('nav-list');
     navList.style.marginTop = navList.style.marginTop == '0px' ? '-310px' : '0px'; 
 };
+
+setTimeout(function(){
+	document.getElementById('messages').style.opacity = 0;
+}, 3000);
