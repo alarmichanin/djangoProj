82bf572d7f009aaf18420215c073c6b42f456079
Vitala Kovalov <vitalya.kovalov@gmail.com>
2021-12-22 02:25:44 +0200

    menu fix

--- static/base/js/base.js
+++ static/base/js/base.js
@@ -1,12 +1,20 @@
 M.AutoInit();
-
 M.updateTextFields();
 
+const navList = document.getElementById('nav-list');
+
+if (document.documentElement.clientWidth >= 997) {
+    navList.style.marginTop = '0px';
+} 
+
 document.getElementById('nav-burger-menu').onclick = () => {
-    const navList = document.getElementById('nav-list');
-    navList.style.marginTop = navList.style.marginTop == '0px' ? '-310px' : '0px'; 
+    navList.style.marginTop = navList.style.marginTop == '0px' ? '-680px' : '0px'; 
 };
 
+if (!Array.from(document.getElementById('messages').children).length) {
+    document.getElementById('messages').style.display = 'none';
+}
+    
 setTimeout(function(){
 	document.getElementById('messages').style.opacity = 0;
 }, 3000);
