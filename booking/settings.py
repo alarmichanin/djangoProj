85f19309f4003d2e3207393dcc641c69b262d11b
YaroslavYatyk <duhanov2003@gmail.com>
2021-12-22 11:25:12 +0200

    try to fix mail sender

--- booking/settings.py
+++ booking/settings.py
@@ -141,14 +141,13 @@ else:
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.googlemail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "bookingdjangokpi@gmail.com"
-EMAIL_HOST_PASSWORD = "romavitalayarikvasya"
+EMAIL_HOST_USER = "duhanov2003@gmail.com"
+EMAIL_HOST_PASSWORD = "proshnik31"
 EMAIL_USE_TLS = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-ALLOWED_HOSTS = ['*']
